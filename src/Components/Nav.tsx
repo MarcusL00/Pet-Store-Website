766893a205bac87c77fa3b9a368b58d0ade01b90
MarcusL00 <118048523+MarcusL00@users.noreply.github.com>
2024-03-19 08:18:51 +0100

    navbar
    had alot of trouble with navbar but its bussin now, poggers
    
    Co-Authored-By: Malina <144940854+voxpin@users.noreply.github.com>

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-scroll";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
+        <div className="flex items-center flex-1">
+          <span className="text-3xl font-bold">Logo</span>
+        </div>
+        <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
+          <div className="flex-10">
+            <ul className="flex gap-8 mr-16 text-[18px]">
+              <li>
+                <Link to="Home">Home</Link>
+              </li>
+              <li>
+                <Link to="About">About</Link>
+              </li>
+              <li>
+                <Link to="Products">Products</Link>
+              </li>
+              <li>
+                <Link to="Contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
