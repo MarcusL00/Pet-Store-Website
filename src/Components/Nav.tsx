96094de44c29325cfbbb64abc172da0495c75f46
MarcusL00 <118048523+MarcusL00@users.noreply.github.com>
2024-05-06 12:59:31 +0200

    huge

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
+import {FaTimes} from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
 
 const Nav = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click)
+  }
+  const content = (
+    <> 
+      <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
+        <ul className="text-center text-xl p-20">
+          <li className=" my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
+            <Link spy={true} smooth={true} to="Home">Home</Link>
+          </li>
+          <li className=" my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
+            <Link spy={true} smooth={true} to="About">About</Link>
+          </li>
+          <li className=" my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
+            <Link spy={true} smooth={true} to="Products">Products</Link>
+          </li>
+          <li className=" my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
+            <Link spy={true} smooth={true} to="Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
   return (
     <nav>
       <div className="h-10vh flex justify-between z-50 text-gray-600 lg:py-5 px-20 py-4">
@@ -10,21 +37,35 @@ const Nav = () => {
         <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
-              <li>
-                <Link to="Home">Home</Link>
-              </li>
-              <li>
-                <Link to="About">About</Link>
-              </li>
-              <li>
-                <Link to="Products">Products</Link>
-              </li>
-              <li>
-                <Link to="Contact">Contact</Link>
-              </li>
+              <Link spy={true} smooth={true} to="Home">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Home
+                </li>
+              </Link>
+              <Link spy={true} smooth={true} to="About">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  About
+                </li>
+              </Link>
+              <Link spy={true} smooth={true} to="Products">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Products
+                </li>
+              </Link>
+              <Link spy={true} smooth={true} to="Contact">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Contact
+                </li>
+              </Link>
             </ul>
           </div>
         </div>
+        <div>
+          {click && content}
+        </div>
+        <button className="block sm:hidden transition" onClick={handleClick}>
+          {click ? <FaTimes/> : <CiMenuFries/>}
+        </button>
       </div>
     </nav>
   );
--- src/Components/Products.tsx
+++ src/Components/Products.tsx
@@ -2,7 +2,7 @@ const Product = () => {
 
     return (
 
-    <div className="mx-auto mt-11 w-64 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+    <div className="-z-10 mx-auto mt-11 w-64 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
         <img className="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1583096985230-7cbff05f1bba?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Product Image" />
             <div className="p-4">
                 <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">Hund</h2>
